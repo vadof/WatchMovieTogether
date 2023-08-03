4a87b2e3aed28d86a2276ede35e6134f73e1060e
Vadim <vafilo@ttu.ee>
2023-08-03 17:09:46 +0300

    parse series into object

--- HdRezkaApi/Entities.py
+++ HdRezkaApi/Entities.py
@@ -31,8 +31,7 @@ class Translation:
         }
 
     def __str__(self):
-        return 'name=' + self.name + ',' + \
-            'resolutions=' + self.resolutions
+        return f'name={self.name}, resolutions={self.resolutions}'
 
 
 class Resolution:
@@ -43,4 +42,56 @@ class Resolution:
         return {'value': self.value}
 
     def __str__(self):
-        return 'value=' + self.value
+        return f'value={self.value}'
+
+
+class Series:
+
+    def __init__(self, link, name, seriesTranslations):
+        self.link = link
+        self.name = name
+        self.seriesTranslations = seriesTranslations
+
+    def to_dict(self):
+        return {
+            'link': self.link,
+            'name': self.name,
+            'seriesTranslations': [s.to_dict() for s in self.seriesTranslations]
+        }
+
+    def __str__(self):
+        return f'link={self.link}, name={self.name}, seriesTranslations={self.seriesTranslations}'
+
+
+class SeriesTranslation:
+
+    def __init__(self, name, seasons, resolutions):
+        self.name = name
+        self.seasons = seasons
+        self.resolutions = resolutions
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'seasons': [s.to_dict() for s in self.seasons],
+            'resolutions': [r.to_dict() for r in self.resolutions],
+        }
+
+    def __str__(self):
+        return f'name={self.name}, seasons={self.seasons}, resolutions={self.resolutions}'
+
+class Season:
+
+    def __init__(self, number, episodes):
+        self.number = number
+        self.episodes = episodes
+
+
+    def to_dict(self):
+        return {
+            'number': self.number,
+            'episodes': self.episodes
+        }
+
+    def __str__(self):
+        return f'number={self.number}, episodes={self.episodes}'
--- HdRezkaApi/main.py
+++ HdRezkaApi/main.py
@@ -3,7 +3,7 @@ from aifc import Error
 from flask import Flask, jsonify, request, make_response
 from HdRezkaAPI import HdRezkaApi
 from flask_cors import CORS
-from Entities import Movie, Translation, Resolution
+from Entities import *
 
 
 app = Flask(__name__)
@@ -17,6 +17,20 @@ def getMovie():
     try:
         movie = getMovieObject(url)
         return make_response(jsonify(movie=movie.to_dict()), 200)
+    except:
+        return make_response(jsonify(error='Invalid URL provided.'), 400)
+
+
+@app.route('/api/movie/link', methods=['POST'])
+def getStreamLink():
+    data = request.get_json()
+    url = data.get('url')
+    translation = data.get('translation')
+    resolution = data.get('resolution')
+    try:
+        rezka = HdRezkaApi(url)
+        stream = rezka.getStream(translation=translation)(resolution)
+        return make_response(jsonify(stream, 200))
     except:
         return make_response(jsonify(error='Invalid URL provided.'), 400)
 
@@ -41,18 +55,33 @@ def getMovieObject(url, attempts=3):
             return None
 
 
-@app.route('/api/movie/link', methods=['POST'])
-def getStreamLink():
-    data = request.get_json()
-    url = data.get('url')
-    translation = data.get('translation')
-    resolution = data.get('resolution')
+def getSeriesObject(url, attempts=3):
     try:
         rezka = HdRezkaApi(url)
-        stream = rezka.getStream(translation=translation)(resolution)
-        return make_response(jsonify(stream, 200))
+
+        seriesInfo = rezka.getSeasons()
+        translations = seriesInfo.keys()
+        seriesTranslations = []
+
+        for t in translations:
+            translationInfo = seriesInfo.get(t)
+            translationSeasons = []
+            episodes = translationInfo.get('episodes')
+            for s in translationInfo.get('seasons').keys():
+                season = Season(s, len(episodes.get(s)))
+                translationSeasons.append(season)
+
+            resolutions = list(rezka.getStream(list(translationInfo.get('seasons').keys())[0], '1', t).videos.keys())
+            translation = SeriesTranslation(t, translationSeasons, resolutions)
+
+            seriesTranslations.append(translation)
+
+        return Series(url, rezka.getName(), seriesTranslations)
     except:
-        return make_response(jsonify(error='Invalid URL provided.'), 400)
+        if (attempts - 1 > 0):
+            return getSeriesObject(url, attempts - 1)
+        else:
+            return None
 
 
 if __name__ == '__main__':
